212ecb8b89f5c5214e15c66e7c13da9660dba9ed
Syed Baqir Raza <sbaqarskp786@gmail.com>
2021-06-23 21:02:20 +0500

    Restful API

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
 
 var productSchema = mongoose.Schema({
   name: String,
@@ -7,4 +8,13 @@ var productSchema = mongoose.Schema({
 
 var Product = mongoose.model("Product", productSchema);
 
-module.exports = Product;
+function validateProduct(data) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(10).required(),
+    price: Joi.number().min(0).required(),
+  });
+  return schema.validate(data, { abortEarly: false });
+}
+
+module.exports.Product = Product;
+module.exports.validate = validateProduct;
--- routes/api/products.js
+++ routes/api/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 let router = express.Router();
-
-var Product = require("../../models/product");
+const validateProduct = require("../../middlewares/validateProduct");
+var { Product } = require("../../models/product");
 
 //GET all products
 router.get("/", async (req, res) => {
@@ -21,6 +21,34 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.put("/", async (req, res) => {});
+//update a product
+router.put("/:id", validateProduct, async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  product.name = req.body.name;
+  product.price = req.body.price;
+  await product.save();
+  return res.send(product);
+});
+
+//delete a product
+router.delete("/:id", async (req, res) => {
+  try {
+    let product = await Product.findByIdAndDelete(req.params.id);
+    if (!product)
+      return res.status(400).send("Product with given ID already deleted"); //product with given ID not present
+    return res.send(product); //everything is ok
+  } catch (err) {
+    return res.status(400).send("Invalid ID"); //format of ID is not correct
+  }
+});
+
+//Insert a record
+router.post("/", validateProduct, async (req, res) => {
+  let product = new Product();
+  product.name = req.body.name;
+  product.price = req.body.price;
+  await product.save();
+  return res.send(product);
+});
 
 module.exports = router;
